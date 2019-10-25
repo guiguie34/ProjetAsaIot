8b7f1c4c5dacf22ad0e733677bf92bf6b5c43314
Guillaume Chebib <guillaume.chebib@hotmail.fr>
2019-10-25 23:38:00 +0200

    Ajout des ports

---
+++ TP/README.md
@@ -0,0 +1,6 @@
+#Branchement des capteurs/actuateurs:
+#Potentiomètre A0
+#Ecran LCD I2C-1
+#Capteur Ultrason D4
+#LED D5
+#Capteur Poids ?
--- TP/utils/LED.py
+++ TP/utils/LED.py
@@ -2,7 +2,7 @@ import time
 from grovepi import *
 
 # Connect the Grove LED to digital port D5
-led = 4
+led = 5
 
 def allume():
 	return digitalWrite(led,1)
