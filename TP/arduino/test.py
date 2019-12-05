d87dc1ed6a53fb8cf3bd9a96c93b543471e4a78f
Guillaume Chebib <guillaume.chebib@hotmail.fr>
2019-12-06 00:18:28 +0100

    ajout

--- TP/arduino/test.py
+++ TP/arduino/test.py
@@ -1,6 +1,11 @@
-import serial 
+import serial
+from driverI2C import *
+import time
+import os
 
-serialArduino= serial.Serial('/dev/ttyACM0', 9600)
-
+serialArduino= serial.Serial('/dev/ttyACM0', 38400)
+#os.system('i2cset -y 1 0x3e 0x80 0x01')
 while True:
-	print(serialArduino.readline())
+	setRGB(255,255,0)
+	setText(serialArduino.readline())
+
